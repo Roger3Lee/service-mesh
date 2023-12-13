cd99d68d8d7048c59e3a7c561e92c03f5e3c71ef
zhujia2 <jia2.zhu@outlook.com>
2023-12-13 21:28:39 +0800

    添加后端代码

---
+++ service-mesh-be/service-mesh-boot/src/main/java/com/artframework/Bootstrap.java
@@ -0,0 +1,15 @@
+package com.artframework.servicemesh;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@MapperScan(basePackages = "com.artframework.sample.mappers")
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+}
---
+++ service-mesh-be/service-mesh-boot/src/main/java/com/artframework/servicemesh/config/MybatisPlusConfiguration.java
@@ -0,0 +1,25 @@
+package com.artframework.servicemesh.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author li.pengcheng
+ * @version V1.0
+ * @date 2023/10/8
+ **/
+@Configuration
+public class MybatisPlusConfiguration {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        return interceptor;
+    }
+}
