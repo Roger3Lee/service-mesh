029f02a8269788f82dcc3a1e620aa0ed76777dcc
Roger li <li.pengcheng5@iwhilecloud.com>
2023-12-26 14:44:57 +0800

    提交服務編排設計的代碼

--- service-mesh-be/service-mesh-boot/src/main/java/com/artframework/servicemesh/domains/datasource/repository/impl/DatasourceRepositoryImpl.java
+++ service-mesh-be/service-mesh-boot/src/main/java/com/artframework/servicemesh/domains/datasource/repository/impl/DatasourceRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.artframework.servicemesh.domains.datasource.repository.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.collection.ListUtil;
+import cn.hutool.core.util.StrUtil;
 import com.artframework.servicemesh.domains.datasource.convertor.*;
 import com.artframework.servicemesh.domains.datasource.lambdaexp.*;
 import com.artframework.servicemesh.domains.datasource.dto.*;
@@ -13,6 +16,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import org.springframework.stereotype.Repository;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import org.springframework.util.StringUtils;
 
 import java.io.Serializable;
 import java.util.List;
@@ -44,6 +48,9 @@ public class DatasourceRepositoryImpl extends BaseRepositoryImpl<DatasourceDTO,S
     public IPage<DatasourceDTO> page(DatasourcePageRequest request){
         IPage<SvcMeshDatasourceDO> page=new Page<>(request.getPageNum(), request.getPageSize());
         LambdaQueryWrapper<SvcMeshDatasourceDO> wrapper =new LambdaQueryWrapper<SvcMeshDatasourceDO>();
+        wrapper.like(StrUtil.isNotBlank(request.getCode()), SvcMeshDatasourceDO::getCode, request.getCode());
+        wrapper.like(StrUtil.isNotBlank(request.getName()), SvcMeshDatasourceDO::getName, request.getName());
+        wrapper.in(CollectionUtil.isNotEmpty(request.getType()), SvcMeshDatasourceDO::getType, request.getType());
         return this.baseMapper.selectPage(page,wrapper).convert(DatasourceConvertor.INSTANCE::convert2DTO);
     }
 
